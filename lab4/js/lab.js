51801898a174e0b0aff5fed1bc4d5aee93bcc33a
sma <srojasbusiness@gmail.com>
2025-04-21 17:56:47 -0700

    lab4 overhaul

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,8 +1,20 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Lab4: Checkers Pseudocode.js
+// A psudocode of the game, Checkers.
+// Author: Sam & Cesar
+// Date: April 2025
 
-// Constants
+// Checkers Pseudocode Tasks:
+// To print the board for each turn. 
+// Add all the pieces for the entire game. 
+// Players decide who is team red and team black.
+// Flip a coin to decide who will go first.
+// Whoever wins the coin flips is able to move their checker piece first diagonally.
+// The second will then make the next diagonal move.
+// Repeat turns to capture your opponent’s checker pieces by jumping over them.
+  // If a player’s checker reaches the end of the board it will become a king piece.
+    // A King Piece will be able to move diagonally backwards and forwards.
+// A winning player is decided once a player captures all of the checker pieces of the opposing player.
+
 
 // Functions
 
