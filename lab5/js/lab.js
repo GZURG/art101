cf49faeef14c715312eea012635c1ba759a22004
sma <srojasbusiness@gmail.com>
2025-04-24 14:34:10 -0700

    lab 5 overhaul

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,27 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Lab 5: Data Types and Variables
+// Author: Sam Rojas
+// Date: April 2025
 
-// Constants
 
-// Functions
+// Variables
+make = 'Nissan';
+model = 'Frontier';
+color = 'black';
+year = '2015';
+
+
+// Calculate
+age = 2025 - year;
+
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
